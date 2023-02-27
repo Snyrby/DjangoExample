fcb080f1ac6d391acbde2960262b2239ae1ca463
Shawn Ruby <snyrby@gmail.com>
2023-02-27 12:10:24 -0800

    More progress

--- pages/views.py
+++ pages/views.py
@@ -12,4 +12,5 @@ def home_view(request, *args, **kwargs): # *args, **kwargs
     return render(request, 'home.html', {})
 
 def contact_view(request, *args, **kwargs):
-    return render(request, 'contact.html', {})
+    my_context = {'my_text': 'This is about us', 'my_number': 123,  'my_list': [123, 4242, 12313]}
+    return render(request, 'contact.html', my_context)
