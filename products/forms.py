2aa470d7c7c8e2880ebacb7af8d7bb690a6a27aa
Shawn Ruby <snyrby@gmail.com>
2023-03-02 06:38:13 -0800

    progress

--- products/forms.py
+++ products/forms.py
@@ -3,6 +3,11 @@ from .models import Product
 
 class ProductForm(forms.ModelForm):
     title = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Your Title'}))
+    description = forms.CharField(required=False, widget=forms.Textarea(attrs={'placeholder': 'Your Description',
+                                                                               'class': 'new-class-name two',
+                                                                               'id': 'my-id-for-text area', 'rows': 20,
+                                                                               'cols': 120}))
+    price = forms.DecimalField(initial=199.99)
     class Meta:
         model = Product
         fields = ['title', 'description', 'price']
