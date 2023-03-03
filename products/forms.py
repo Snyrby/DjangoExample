eafeb09152deac2d93ed0173f8506831189b75b4
Shawn Ruby <snyrby@gmail.com>
2023-03-02 16:05:22 -0800

    more process

--- products/forms.py
+++ products/forms.py
@@ -3,6 +3,7 @@ from .models import Product
 
 class ProductForm(forms.ModelForm):
     title = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Your Title'}))
+    email = forms.EmailField()
     description = forms.CharField(required=False, widget=forms.Textarea(attrs={'placeholder': 'Your Description',
                                                                                'class': 'new-class-name two',
                                                                                'id': 'my-id-for-text area', 'rows': 20,
@@ -11,6 +12,18 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['title', 'description', 'price']
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get('title')
+        if not 'CFE' in title:
+            raise forms.ValidationError('This is not a valid title')
+        return title
+    def clean_email(self, *args, **kwargs):
+        email = self.cleaned_data.get('email')
+        if not email.endswith('edu'):
+            raise forms.ValidationError('This is not a valid email')
+        return email
+
+
 
 
 class RawProductForm(forms.Form):
